7e43442d923bd0dd6d255f69ba8adfee61571e74
POTHULA. JAI SAI <92910772+jaisaipothula@users.noreply.github.com>
2025-03-21 22:01:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,25 @@
-const makeChange = (c) => {
-  // your name here
-};
+function makeChange(amount) {
+  let change = {
+    q: 0, // Quarters
+    d: 0, // Dimes
+    n: 0, // Nickels
+    p: 0  // Pennies
+  };
 
-// Do not the change the code below
-const c = prompt("Enter c: ");
-alert(JSON.stringify(makeChange(c)));
+  // Calculate the number of quarters
+  change.q = Math.floor(amount / 25);
+  amount = amount % 25;
+
+  // Calculate the number of dimes
+  change.d = Math.floor(amount / 10);
+  amount = amount % 10;
+
+  // Calculate the number of nickels
+  change.n = Math.floor(amount / 5);
+  amount = amount % 5;
+
+  // The remaining amount is in pennies
+  change.p = amount;
+
+  return change;
+}
